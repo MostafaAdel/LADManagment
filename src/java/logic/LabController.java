75db545f05b27d2ac965abeda1df09a80cabe134
HossamAbdallah <hossamabdalh@gmail.com>
2015-03-23 21:21:19 -0700

    resolving error in git

--- src/java/logic/LabController.java
+++ src/java/logic/LabController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -18,24 +18,3 @@ public class LabController {
 
     
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package logic;
-
-import dao.instructor.LabDaoImpl;
-import dto.instructor.StudentDto;
-import pojo.Lab;
-
-/**
- *
- * @author Mostafa_ITI
- */
-public class LabController {
-
-    
-}
->>>>>>> 47d89df5014cd689881c6673ab374229c08df3d6
